88a4bd32082eb2030c0e102dfc6c0cb91066a16e
Franklin Hiciano <fhiciano0@gmail.com>
2025-10-22 13:25:51 -0400

    added franken download code

--- results/2025-09-12_HGSV3_ig_tcr_data/run.sh
+++ results/2025-09-12_HGSV3_ig_tcr_data/run.sh
@@ -2,10 +2,12 @@
 set -e -x
 set -euo pipefail
 
-THREADS=12 #same as bsub
+THREADS=24 #same as bsub
 data=/sc/arion/scratch/hiciaf01/projects/imputation/data/2025-10-07_1KG_short_long
-reference_dir=${data}/hg38_reference
+hg38_reference_dir=${data}/hg38_reference
+franken_reference_dir=${data}/franken_reference
 assemblies_dir=${data}/assemblies/assemblies
+paf_dir=${data}/paf
 
 function get_assemblies {
 	pass	# acquired through globus
@@ -41,35 +43,40 @@ function count_contigs_fai {
 }
 
 function download_hg38 {
-	wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/000/001/405/GCA_000001405.15_GRCh38/seqs_for_alignment_pipelines.ucsc_ids/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna.gz -O ${data}/hg38_reference/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna.gz 
+	mkdir -p ${hg38_reference_dir}
+	wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/000/001/405/GCA_000001405.15_GRCh38/seqs_for_alignment_pipelines.ucsc_ids/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna.gz -O ${hg38_reference_dir}/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna.gz 
 }
 
 function index_hg38_with_minimap2 {
 	module load minimap2
-	mkdir -p ${reference_dir}
-	minimap2 -d ${reference_dir}/GRCh38_no_alt.mmi ${reference_dir}/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna.gz
+	minimap2 -d ${hg38_reference_dir}/GRCh38_no_alt.mmi ${hg38_reference_dir}/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna.gz
 }
 
 
 function align_assemblies {
 	module load minimap2
-	outdir=${data}/aligned_assemblies
-	mkdir -p ${outdir}
+	mkdir -p ${paf_dir}
+	
 	for hap in hap1 hap2
 do
-	all_assemblies=$(ls -d ${data}/assemblies/assemblies/*/*${hap}.fasta)
+	all_assemblies=$(ls -d ${assemblies_dir}/*/*${hap}.fasta)
 	while read assembly
 	do
-		minimap2 -x asm5 -t "$THREADS" -c --cs=long --secondary=no "${data}/hg38_reference/GRCh38_no_alt.mmi" "${assembly}" > "${outdir}/$(basename ${assembly}).paf"
+		echo "RUNNING ASSEMBLY ${assembly}"
+		minimap2 -x asm5 -t "$THREADS" -c --secondary=no "${data}/hg38_reference/GRCh38_no_alt.mmi" "${assembly}" > "${paf_dir}/$(basename ${assembly}).paf"
 	done <<< "${all_assemblies}"
 done
 }
 
-
+function download_franken_reference {
+	
+	mkdir ${franken_reference_dir}
+	wget http://immunogenomics.louisville.edu/immune_receptor_genomics/current/reference.fasta -O ${franken_reference_dir}
+}
 
 #get_assemblies
-count_contigs_fai
+#count_contigs_fai
 #download_hg38
 #index_hg38_with_minimap2
 #align_assemblies
-
+download_franken_reference
