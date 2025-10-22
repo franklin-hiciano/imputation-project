4982804daf4da12b614e3488a90fc242197de420
Franklin Hiciano <fhiciano0@gmail.com>
2025-10-22 12:12:51 -0400

    generated contig counts

--- results/2025-09-12_HGSV3_ig_tcr_data/run.sh
+++ results/2025-09-12_HGSV3_ig_tcr_data/run.sh
@@ -2,59 +2,43 @@
 set -e -x
 set -euo pipefail
 
-assemblies_dir=/sc/arion/scratch/hiciaf01/projects/imputation/data/2025-10-07_1KG_short_long/assemblies/assemblies
-sample_assembly_names=$(ls ${assemblies_dir}/)
-
+THREADS=12 #same as bsub
 data=/sc/arion/scratch/hiciaf01/projects/imputation/data/2025-10-07_1KG_short_long
+reference_dir=${data}/hg38_reference
+assemblies_dir=${data}/assemblies/assemblies
 
 function get_assemblies {
 	pass	# acquired through globus
 }
 
-function count_contigs {
-	sample=HG00096
-	gunzip -c ${assemblies_dir}/${sample}/${sample}.vrk-ps-sseq.asm-hap1.fasta.gz | grep -c ">"
-	gunzip -c ${assemblies_dir}/${sample}/${sample}.vrk-ps-sseq.asm-hap2.fasta.gz | grep -c ">"
+function sum_contigs_per_sample {
+  infile="contig_counts.tsv"
+  outfile="contig_counts_summed.tsv"
+  echo -e "sample\ttotal_contigs" > ${outfile}
+
+  # Use awk to sum hap1 + hap2 per sample
+  awk -F'\t' '
+  {
+    counts[$1] += $3
+  }
+  END {
+    for (sample in counts)
+      printf "%s\t%d\n", sample, counts[sample]
+  }' ${infile} | sort -k1,1 >> ${outfile}
 }
-
-function count_contigs_multithreaded {
-	sample=HG00096
-	copy=${assemblies_dir}/${sample}/${sample}.vrk-ps-sseq.asm-hap1_copy.fasta.gz
-	f=${assemblies_dir}/${sample}/${sample}.vrk-ps-sseq.asm-hap1_copy.fasta
-	cp ${assemblies_dir}/${sample}/${sample}.vrk-ps-sseq.asm-hap1.fasta.gz ${copy}
-	unpigz ${copy}
-	grep -c ">" ${f}
-}
-
-function count_all_contigs_multithreaded {
-	counts=${assemblies_dir}/contig_counts.txt
-	touch ${counts}
-	echo "" > ${counts}
-	for hap in hap1 hap2
-do
-	all_assemblies=$(ls -d /sc/arion/scratch/hiciaf01/projects/imputation/data/2025-10-07_1KG_short_long/assemblies/assemblies/*/*${hap}.fasta.gz)
-	while read assembly
-	do
-		unpigz -kf ${assembly}
-		num_contigs=$(grep -c "^>" ${assembly%.gz})
-		printf "%s\t%s\n" "${assembly}" "${num_contigs}" >> "$counts"
-
-	done <<< "${all_assemblies}"
-done
-}
-
+	
 function count_contigs_fai {
 	ls ${data}/assemblies/assemblies | while read sample
 	do
+		
 		for hap in hap1 hap2
 		do
 			num=`cat ${data}/assemblies/assemblies/${sample}/${sample}.vrk-ps-sseq.asm-${hap}.fasta.gz.fai | wc -l`
-			echo "${sample}\t${hap}\t${num}"
+			printf "%s\t%s\t%s\n" "${sample}" "${hap}" "${num}"
 		done
-	done > count.txt
+	done > contig_counts.tsv
+	sum_contigs_per_sample
 }
-	
-
 
 function download_hg38 {
 	wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/000/001/405/GCA_000001405.15_GRCh38/seqs_for_alignment_pipelines.ucsc_ids/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna.gz -O ${data}/hg38_reference/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna.gz 
@@ -62,33 +46,30 @@ function download_hg38 {
 
 function index_hg38_with_minimap2 {
 	module load minimap2
-	minimap2 -d ${data}/hg38_reference/GRCh38_no_alt.mmi ${data}/hg38_reference/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna
+	mkdir -p ${reference_dir}
+	minimap2 -d ${reference_dir}/GRCh38_no_alt.mmi ${reference_dir}/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna.gz
 }
-
-function align_reads {
-
-minimap2 -ax map-ont GRCh38_no_alt.mmi reads.fastq > aln.sam
 
 
 function align_assemblies {
+	module load minimap2
 	outdir=${data}/aligned_assemblies
-	mkdir outdir
+	mkdir -p ${outdir}
 	for hap in hap1 hap2
 do
-	all_assemblies=$(ls -d ${data}/assemblies/assemblies/*/*${hap}.fasta.gz)
+	all_assemblies=$(ls -d ${data}/assemblies/assemblies/*/*${hap}.fasta)
 	while read assembly
 	do
 		minimap2 -x asm5 -t "$THREADS" -c --cs=long --secondary=no "${data}/hg38_reference/GRCh38_no_alt.mmi" "${assembly}" > "${outdir}/$(basename ${assembly}).paf"
-	done 
+	done <<< "${all_assemblies}"
 done
 }
 
 
 
-#count_assemblies
-#count_contigs
-#count_contigs_multithreaded
-#count_all_contigs_multithreaded
+#get_assemblies
+count_contigs_fai
 #download_hg38
-index_hg38_with_minimap2
+#index_hg38_with_minimap2
 #align_assemblies
+
