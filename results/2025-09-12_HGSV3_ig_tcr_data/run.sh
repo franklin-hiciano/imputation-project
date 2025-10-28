05dde56e6555f2252526342dabf4d1768085fcef
Franklin Hiciano <fhiciano0@gmail.com>
2025-10-28 15:19:39 -0400

    removed variables

--- results/2025-09-12_HGSV3_ig_tcr_data/run.sh
+++ results/2025-09-12_HGSV3_ig_tcr_data/run.sh
@@ -5,31 +5,20 @@ set -euo pipefail
 data=/sc/arion/scratch/hiciaf01/projects/imputation/data/2025-10-07_1KG_short_long
 results=/sc/arion/work/hiciaf01/projects/imputation/results/2025-09-12_HGSV3_ig_tcr_data
 
+mkdir -p ${data}/long_reads
+mkdir -p ${data}/fastq1
+mkdir -p ${data}/fastq2
+mkdir -p ${data}/hg38_reference
+mkdir -p ${data}/paf_using_correct_hg38
+mkdir -p ${data}/franken_reference_dir
 
-hg38_reference_dir=${data}/hg38_reference
-franken_reference_dir=${data}/franken_reference
-assemblies_dir=${data}/assemblies/assemblies
-paf_dir=${data}/paf_using_correct_hg38
-fastq_read1_dir=${data}/fastq1
-fastq_read2_dir=${data}/fastq2
-long_reads_index=/sc/arion/work/hiciaf01/projects/imputation/results/2025-09-12_HGSV3_ig_tcr_data/igsr_HGSVC3.tsv.tsv
-long_reads_dir=${data}/long_reads
-
-
-mkdir -p ${long_reads_dir}
-mkdir -p ${fastq_read1_dir}
-mkdir -p ${fastq_read2_dir}
-mkdir -p ${hg38_reference_dir}
-mkdir -p ${paf_dir}
-mkdir -p ${franken_reference_dir}
-
-mapfile -t assemblies < <(find "${assemblies_dir}" -maxdepth 2 -mindepth 2 -name '*.vrk-ps-sseq.asm-hap[12].fasta.gz')
+mapfile -t assemblies < <(find "${data}/assemblies/assemblies" -maxdepth 2 -mindepth 2 -name '*.vrk-ps-sseq.asm-hap[12].fasta.gz')
 
 function count_contigs {
     assemblies_dir="${data}/assemblies/assemblies"
     individual_counts_file="contig_counts.tsv"
 
-    mapfile -t fai_files < <(find "${assemblies_dir}" -maxdepth 2 -mindepth 2 -name '*.vrk-ps-sseq.asm-hap[12].fasta.gz.fai')
+    mapfile -t fai_files < <(find "${data}/assemblies/assemblies" -maxdepth 2 -mindepth 2 -name '*.vrk-ps-sseq.asm-hap[12].fasta.gz.fai')
     
     echo -e "sample\thap\tcontigs" > "${individual_counts_file}"
     
@@ -61,70 +50,37 @@ function sum_contigs_per_sample {
 
 function download_hg38 {
 	url=https://ftp.1000genomes.ebi.ac.uk/vol1/ftp/technical/reference/GRCh38_reference_genome/GRCh38_full_analysis_set_plus_decoy_hla.fa
-        wget ${url} -O ${hg38_reference_dir}/GRCh38_full_analysis_set_plus_decoy_hla.fa
+        wget ${url} -O ${data}/hg38_reference/GRCh38_full_analysis_set_plus_decoy_hla.fa
 }
 
 function index_hg38_with_minimap2 {
 	module load minimap2
-        minimap2 -d ${hg38_reference_dir}/GRCh38_full_analysis_set_plus_decoy_hla.mmi ${hg38_reference_dir}/GRCh38_full_analysis_set_plus_decoy_hla.fa
+        minimap2 -d ${data}/hg38_reference/GRCh38_full_analysis_set_plus_decoy_hla.mmi ${data}/hg38_reference/GRCh38_full_analysis_set_plus_decoy_hla.fa
 }
-
-function align_assemblies {
-
-  local num_cores=16
-
-  for assembly_file in "${assemblies[@]}"; do
-
-    local fname=$(basename "${assembly_file}")
-    local job_name="${fname%.fasta.gz}"
-    local paf_output="${paf_dir}/${job_name}.paf"
-
-    local bsub_command="module load minimap2 && \
-      minimap2 -x asm5 -t ${num_cores} -c --secondary=no \
-      ${hg38_reference_dir}/GRCh38_full_analysis_set_plus_decoy_hla.mmi \"${assembly_file}\" \
-      > \"${paf_output}\""
-
-    echo "Submitting ${job_name}..."
-    bsub -J "${job_name}" \
-      -P "acc_oscarlr" \
-      -n "${num_cores}" \
-      -R "span[hosts=1]" \
-      -R "rusage[mem=8000]" \
-      -q express \
-      -W 12:00 \
-      -o "${paf_dir}/${job_name}.out" \
-      -e "${paf_dir}/${job_name}.err" \
-      "${bsub_command}"
-
-  done
-}
-
-
 function align_assemblies_oscar {
 
 
   cat contig_counts.tsv | while read sample hap contigs
   do
-	assembly_file=
-    local fname=$(basename "${assembly_file}")
-    local job_name="${fname%.fasta.gz}"
-    local paf_output="${paf_dir}/${job_name}.paf"
+	fname=$(basename ${data}/assemblies/assemblies/${sample}.vrk-ps-sseq.asm-hap${hap}.fasta.gz)
+	job_name="${fname%.fasta.gz}"
+    	paf_output="${data}/paf_using_correct_hg38/${job_name}.paf"
 
-    local bsub_command="module load minimap2 && \
-      minimap2 -x asm5 -t 12 -c --secondary=no \
-      ${hg38_reference_dir}/GRCh38_full_analysis_set_plus_decoy_hla.mmi \"${data}/assemblies/assemblies/${sample}.vrk-ps-sseq.asm-hap${hap}.fasta.gz\" \
+ 	bsub_command="module load minimap2 && \
+      minimap2 -x asm5 -t 16 -c --secondary=no \
+      ${data}/hg38_reference/GRCh38_full_analysis_set_plus_decoy_hla.mmi \"${data}/assemblies/assemblies/${sample}.vrk-ps-sseq.asm-hap${hap}.fasta.gz\" \
       > \"${data}/paf_using_correct_hg38/${sample}.vrk-ps-sseq.asm-hap${hap}.paf\""
 
     echo "Submitting ${job_name}..."
     bsub -J "${job_name}" \
       -P "acc_oscarlr" \
-      -n "12" \
+      -n "16" \
       -R "span[hosts=1]" \
       -R "rusage[mem=8000]" \
       -q express \
       -W 12:00 \
-      -o "${paf_dir}/${job_name}.out" \
-      -e "${paf_dir}/${job_name}.err" \
+      -o "${data}/paf_using_correct_hg38/${job_name}.out" \
+      -e "${data}/paf_using_correct_hg38/${job_name}.err" \
       "${bsub_command}"
 
   done
@@ -151,7 +107,7 @@ function long_read_fofn {
 function make_globus_batch_file {
   while IFS=$'\t' read -r sample path; do
 	path_1000_genomes_endpoint=${path#ftp.sra.ebi.ac.uk}
-	path_minerva=${long_reads_dir}/$(basename -- "${path}")
+	path_minerva=${data}/long_reads/$(basename -- "${path}")
 	path_mssm_arion_endpoint=${path_minerva#/sc/arion}
     	printf "%s\t%s\n" "${path_1000_genomes_endpoint}" "${path_mssm_arion_endpoint}"
   done < long_read_fofn.txt > long_read_batch_transfer.txt
@@ -169,7 +125,7 @@ function get_long_reads_with_globus {
 
 }
 function download_franken_reference {
-	wget http://immunogenomics.louisville.edu/immune_receptor_genomics/current/reference.fasta -O ${franken_reference_dir}/reference.fasta
+	wget http://immunogenomics.louisville.edu/immune_receptor_genomics/current/reference.fasta -O ${data}/franken_reference_dir/reference.fasta
 }
 
 function convert_cram_to_fastq {
@@ -177,7 +133,7 @@ function convert_cram_to_fastq {
 	short_read_crams=$(ls ${data}/*.final.cram)
 	while read cram
 do
-	samtools fastq -1 ${fastq_read1_dir}/$(basename ${cram}).fq -2 ${fastq_read2_dir}/$(basename ${cram}).fq -0 /dev/null -s /dev/null -n ${cram}
+	samtools fastq -1 ${data}/fastq1/$(basename ${cram}).fq -2 ${data}/fastq2/$(basename ${cram}).fq -0 /dev/null -s /dev/null -n ${cram}
 done <<< "${short_read_crams}"
 }
 
