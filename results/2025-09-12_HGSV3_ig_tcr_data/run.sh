77a7523e2555a84350e60eb7d7a908b60498006d
Franklin Hiciano <fhiciano0@gmail.com>
2025-11-07 15:02:41 -0500

    refactored file

--- results/2025-09-12_HGSV3_ig_tcr_data/run.sh
+++ results/2025-09-12_HGSV3_ig_tcr_data/run.sh
@@ -10,10 +10,26 @@ mkdir -p ${data}/long_reads
 mkdir -p ${data}/fastq1
 mkdir -p ${data}/fastq2
 mkdir -p ${data}/hg38_reference
-mkdir -p ${data}/paf_using_correct_hg38
+mkdir -p ${data}/hg38_aligned_to_assemblies
 mkdir -p ${data}/franken_reference_dir
+mkdir -p ${data}/long_reads_paf
 
-mapfile -t assemblies < <(find "${data}/assemblies/assemblies" -maxdepth 2 -mindepth 2 -name '*.vrk-ps-sseq.asm-hap[12].fasta.gz')
+#HUMAN REFERENCE GENOME (HGR)
+function download_hg38 {
+        wget https://ftp.1000genomes.ebi.ac.uk/vol1/ftp/technical/reference/GRCh38_reference_genome/GRCh38_full_analysis_set_plus_decoy_hla.fa -O /sc/arion/work/hiciaf01/databases/references/GRCh38_reference_genome/GRCh38_full_analysis_set_plus_decoy_hla.fa
+}
+
+function subset_hg38 {
+	module load samtools
+	samtools faidx ${databases}/references/GRCh38_reference_genome/GRCh38_full_analysis_set_plus_decoy_hla.fa
+	samtools faidx ${databases}/references/GRCh38_reference_genome/GRCh38_full_analysis_set_plus_decoy_hla.fa chr1 chr2 chr3 chr4 chr5 chr6 chr7 chr8 chr9 chr10 chr11 chr12 chr13 chr14 chr15 chr16 chr17 chr18 chr19 chr20 chr21 chr22 chrX chrY chrM > ${databases}/references/GRCh38_reference_genome/hg38_subset.fa
+		
+}
+
+#REFERENCE GENOMES (RG)
+function get_IG_TCR_loci_on_hg38 {
+	wget https://raw.githubusercontent.com/franklin-hiciano/imputation-project/refs/heads/main/results/2025-09-12_HGSV3_ig_tcr_data/hg38_ig_and_tcr_coordinates.bed -O ${results}/hg38_ig_and_tcr_coordinates.bed
+}
 
 function get_samples {
 	wget https://raw.githubusercontent.com/nefte48/Imputation_data/refs/heads/main/results/2025_08_27_1kg_IG_snps/sample_names_shortR_longR.txt?token=GHSAT0AAAAAADMTK43JVGEXL2SHIJLY2KKA2ICKLYQ -O ${results}/sample_names_shortR_longR.txt
@@ -37,15 +53,6 @@ function count_contigs {
     done >> "${individual_counts_file}"
 }
 
-function count_contigs_within_regions {
-while read sample hap contigs;
-do
-	awk -v OFS='\t' '{print $1, 0, $2}' ${data}/assemblies/assemblies/${sample}/${sample}.vrk-ps-sseq.asm-${hap}.fasta.gz.fai > genome.fai.bed
-
-	bedtools intersect -a hg38_ig_and_tcr_coordinates.bed -b genome.fai.bed -c > ig_tcr_contig_counts_${sample}_${hap}.bed
-done < <(tail -n +2 contig_counts.tsv)
-}
-
 function sum_contigs_per_sample {
     infile="contig_counts.tsv"
     outfile="contig_counts_summed.tsv"
@@ -62,30 +69,11 @@ function sum_contigs_per_sample {
     }' "${infile}" | sort -k1,1 >> "${outfile}"
 }
 
-function download_hg38 {
-        wget https://ftp.1000genomes.ebi.ac.uk/vol1/ftp/technical/reference/GRCh38_reference_genome/GRCh38_full_analysis_set_plus_decoy_hla.fa -O /sc/arion/work/hiciaf01/databases/references/GRCh38_reference_genome/GRCh38_full_analysis_set_plus_decoy_hla.fa
-}
-
-function subset_hg38 {
-	module load samtools
-	samtools faidx ${databases}/references/GRCh38_reference_genome/GRCh38_full_analysis_set_plus_decoy_hla.fa
-	samtools faidx ${databases}/references/GRCh38_reference_genome/GRCh38_full_analysis_set_plus_decoy_hla.fa chr1 chr2 chr3 chr4 chr5 chr6 chr7 chr8 chr9 chr10 chr11 chr12 chr13 chr14 chr15 chr16 chr17 chr18 chr19 chr20 chr21 chr22 chrX chrY chrM > ${databases}/references/GRCh38_reference_genome/hg38_subset.fa
-		
-}
-function index_assemblies {
-        module load minimap2
-        while IFS=$'\t' read sample hap contigs
-do
-	minimap2 -d ${data}/assemblies/assemblies/${sample}/${sample}_${hap}.mmi ${data}/assemblies/assemblies/${sample}/${sample}.vrk-ps-sseq.asm-${hap}.fasta.gz
-done < <(tail -n +2 contig_counts.tsv)
-}
-function align_assemblies_oscar { 
-        
+function align_assemblies { 
         while IFS=$'\t' read sample hap contigs
 do
 	job_name=${sample}_${hap}
-        
-        bsub_command="module load minimap2 && minimap2 -x asm5 -t 16 -c ${data}/assemblies/assemblies/${sample}/${sample}.vrk-ps-sseq.asm-${hap}.fasta.gz ${databases}/references/GRCh38_reference_genome/hg38_subset.fa > ${data}/hg38_aligned_to_assemblies/${sample}_${hap}.paf"
+	bsub_command="module load minimap2 && minimap2 -x asm5 -t 16 -c ${data}/assemblies/assemblies/${sample}/${sample}.vrk-ps-sseq.asm-${hap}.fasta.gz ${databases}/references/GRCh38_reference_genome/hg38_subset.fa > ${data}/hg38_aligned_to_assemblies/${sample}_${hap}.paf"
 
     echo "Submitting ${job_name}..."
     bsub -J "${job_name}" \
@@ -101,7 +89,6 @@ do
 done< <(tail -n +2 contig_counts.tsv)
 }
 
-
 function lift_over {
 	module load minimap2
 	mkdir -p ${data}/lift_over
@@ -111,6 +98,7 @@ function lift_over {
 		ls ${data}/lift_over/${sample}_${hap}.bed
 	done < <(tail -n +2 contig_counts.tsv)
 }
+
 function ig_tcr_regions {
 	module load samtools
 while read sample hap count; do
@@ -118,6 +106,18 @@ while read sample hap count; do
 done < <(tail -n +2 contig_counts.tsv)
 }
 
+function count_contigs_within_regions {
+while read sample hap contigs;
+do
+	awk -v OFS='\t' '{print $1, 0, $2}' ${data}/assemblies/assemblies/${sample}/${sample}.vrk-ps-sseq.asm-${hap}.fasta.gz.fai > genome.fai.bed
+
+	bedtools intersect -a hg38_ig_and_tcr_coordinates.bed -b genome.fai.bed -c > ig_tcr_contig_counts_${sample}_${hap}.bed
+done < <(tail -n +2 contig_counts.tsv)
+}
+
+function download_franken_reference {
+	wget http://immunogenomics.louisville.edu/immune_receptor_genomics/current/reference.fasta -O ${data}/franken_reference_dir/reference.fasta
+}
 
 function combine_assemblies_with_franken_reference {
     while read sample hap contigs
@@ -126,32 +126,19 @@ function combine_assemblies_with_franken_reference {
     done < contig_counts.tsv
 }
 
-function index_new_franken_assemblies {
-	module load minimap2
-	while read sample hap contigs
+
+
+#SHORT READS
+function convert_cram_to_fastq {
+	module load samtools
+	short_read_crams=$(ls ${data}/*.final.cram)
+	while read cram
 do
-        minimap2 -d ${data}/assemblies/assemblies/${sample}.vrk-ps-sseq.asm-${hap}_combined_with_franken.mmi ${data}/assemblies/assemblies/${sample}.vrk-ps-sseq.asm-${hap}_combined_with_franken.fasta
-done < contig_counts.tsv
+	samtools fastq -1 ${data}/fastq1/$(basename ${cram}).fq -2 ${data}/fastq2/$(basename ${cram}).fq -0 /dev/null -s /dev/null -n ${cram}
+done <<< "${short_read_crams}"
 }
 
-function align_long_reads_to_new_franken_assemblies {
-	while read sample hap contigs
-do
-        job_name=${sample}_long_aligned_to_${sample}_${hap}
-        bsub_command="minimap2 -x asm5 -t 16 -c ${data}/assemblies/assemblies/${sample}.vrk-ps-sseq.asm-${hap}_combined_with_franken.fasta ${data}/long_reads/${sample}_joined_long_reads.fasta.gz > ${data}/long_reads_paf/${job_name}.paf"
-	echo "Submitting ${job_name}..."
-	bsub -J "${job_name}" \
-		-P "acc_oscarlr" \
-		-n "16" \
-		-R "span[hosts=1]" \
-		-R "rusage[mem=8000]" \
-		-q express \
-		-W 12:00 \
-		-o "${data}/paf_using_correct_hg38/${job_name}.out" \
-		-e "${data}/paf_using_correct_hg38/${job_name}.err" \
-		"${bsub_command}"
-done < <(tail -n +2 contig_counts.tsv)
-}
+#LONG READS
 function long_read_fofn {
   # builds: sample<TAB>remote_path
   while read -r sample; do
@@ -161,7 +148,32 @@ function long_read_fofn {
   done < sample_names_shortR_longR.txt > long_read_fofn.txt
 }
 
-function cat_long_reads {
+function make_globus_batch_file {
+  while IFS=$'\t' read -r sample path; do
+	path_1000_genomes_endpoint=${path#ftp.sra.ebi.ac.uk}
+	path_minerva=${data}/long_reads/$(basename -- "${path}")
+	path_mssm_arion_endpoint=${path_minerva#/sc/arion}
+    	printf "%s\t%s\n" "${path_1000_genomes_endpoint}" "${path_mssm_arion_endpoint}"
+  done < ${results}/long_read_fofn.txt > ${results}/long_read_batch_transfer.txt
+}
+
+function get_small_globus_batch_file {
+	head -n 10 ${results}/long_read_batch_transfer.txt > ${results}/long_read_batch_transfer_small.txt 
+}
+
+function get_long_reads_with_globus {
+	module load python
+	
+	local SRC="14a0be5f-226c-49fe-b65f-dba083d67fc3"   # source collection UUID
+	local DST="6621ca70-103f-4670-a5a7-a7d74d7efbb7"   # Minerva/Arion collection UUID
+	
+	EMBL_EBI_ENDPOINT=47772002-3e5b-4fd3-b97c-18cee38d6df2   # EMBL-EBI Public Data
+    	MINERVA_ARION_ENDPOINT=6621ca70-103f-4670-a5a7-a7d74d7efbb7
+
+  	globus transfer ${EMBL_EBI_ENDPOINT} ${MINERVA_ARION_ENDPOINT} --label "HGSVC TSV transfer 0001" --batch  ${results}/long_read_batch_transfer_small.txt
+}
+
+function concat_long_reads {
     cat ${results}/sample_names_shortR_longR.txt | while read -r sample
     do
         job_name="${sample}_joined_long_reads"
@@ -177,16 +189,6 @@ function cat_long_reads {
             -e "${data}/long_reads/${job_name}.err" \
             "${bsub_command}"
     done
-    
-}
-
-function make_globus_batch_file {
-  while IFS=$'\t' read -r sample path; do
-	path_1000_genomes_endpoint=${path#ftp.sra.ebi.ac.uk}
-	path_minerva=${data}/long_reads/$(basename -- "${path}")
-	path_mssm_arion_endpoint=${path_minerva#/sc/arion}
-    	printf "%s\t%s\n" "${path_1000_genomes_endpoint}" "${path_mssm_arion_endpoint}"
-  done < ${results}/long_read_fofn.txt > ${results}/long_read_batch_transfer.txt
 }
 
 function align_long_reads_to_new_franken_assemblies {
@@ -207,55 +209,4 @@ do
 		"${bsub_command}"
 done < <(tail -n +2 contig_counts.tsv)
 }
-function get_small_globus_batch_file {
-	head -n 10 ${results}/long_read_batch_transfer.txt > ${results}/long_read_batch_transfer_small.txt 
-}
 
-
-function get_long_reads_with_globus {
-	module load python
-	
-	local SRC="14a0be5f-226c-49fe-b65f-dba083d67fc3"   # source collection UUID
-	local DST="6621ca70-103f-4670-a5a7-a7d74d7efbb7"   # Minerva/Arion collection UUID
-	
-	EMBL_EBI_ENDPOINT=47772002-3e5b-4fd3-b97c-18cee38d6df2   # EMBL-EBI Public Data
-    	MINERVA_ARION_ENDPOINT=6621ca70-103f-4670-a5a7-a7d74d7efbb7
-
-  	globus transfer ${EMBL_EBI_ENDPOINT} ${MINERVA_ARION_ENDPOINT} --label "HGSVC TSV transfer 0001" --batch  ${results}/long_read_batch_transfer_small.txt
-
-}
-function download_franken_reference {
-	wget http://immunogenomics.louisville.edu/immune_receptor_genomics/current/reference.fasta -O ${data}/franken_reference_dir/reference.fasta
-}
-
-function convert_cram_to_fastq {
-	module load samtools
-	short_read_crams=$(ls ${data}/*.final.cram)
-	while read cram
-do
-	samtools fastq -1 ${data}/fastq1/$(basename ${cram}).fq -2 ${data}/fastq2/$(basename ${cram}).fq -0 /dev/null -s /dev/null -n ${cram}
-done <<< "${short_read_crams}"
-}
-
-#get_samples
-#count_contigs
-#sum_contigs_per_sample
-#download_hg38
-#index_hg38_with_minimap2
-#index_assemblies
-#align_assemblies_oscar
-#long_read_fofn
-#make_globus_batch_file
-#get_small_globus_batch_file
-#get_long_reads_with_globus
-#combine_assemblies_with_franken_reference
-#download_hg38
-#align_assemblies_local
-#index_new_franken_assemblies
-#cat_long_reads
-#lift_over
-#subset_hg38
-#ig_tcr_regions
-#count_contigs_within_regions
-#test_lift_over
-#align_long_reads_to_new_franken_assemblies
